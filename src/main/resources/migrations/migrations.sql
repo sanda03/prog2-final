adacf33de6da54dd36afc342eab77b4e5f586d76
sanda03 <hei.sanda.2@gmail.com>
2023-08-25 21:21:22 +0300

    feat: init project

---
+++ src/main/resources/migrations/migrations.sql
@@ -0,0 +1,38 @@
+\c postgres
+
+CREATE DATABASE prog2;
+
+\c prog2
+
+CREATE TABLE "author"(
+  "id_author" SERIAL PRIMARY KEY,
+  "name" VARCHAR(200) NOT NULL,
+  "first_name" VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE "class"(
+  "id_class" SERIAL PRIMARY KEY,
+  "name" VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE "member"(
+  "id_member" SERIAL PRIMARY KEY,
+  "name" VARCHAR(200) NOT NULL,
+  "first_name" VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE "book"(
+  "id_book" SERIAL PRIMARY KEY,
+  "title" VARCHAR(200) NOT NULL,
+  "description" TEXT NOT NULL,
+  "type" VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE "borrow"(
+  "id_borrow" SERIAL PRIMARY KEY,
+  "id_book" INT REFERENCES "book"("id_book") NOT NULL,
+  "id_member" INT REFERENCES "member"("id_member") NOT NULL,
+  "start_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  "end_date" TIMESTAMP NOT NULL,
+  "is_returned" BOOLEAN DEFAULT FALSE NOT NULL
+);
